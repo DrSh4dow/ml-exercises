308fdc6f52b73c81109a2cf5304f4db6e4e3f413
drsh4dow <daniel.morettiv@gmail.com>
2024-05-04 15:40:22 -0400

    implement basic perceptron

--- perceptron/main.py
+++ perceptron/main.py
@@ -1,1 +1,62 @@
-print("Hi there")
+from typing import List
+import numpy as np
+
+
+class Perceptron:
+    """Perceptron classifier.
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    b_ : Scalar
+      Bias unit after fitting.
+    errors_ : list
+      Number of misclassifications (updates) in each epoch.
+    """
+
+    eta: float
+    """Learning rate"""
+
+    n_iter: int
+    """Passes over the training dataset."""
+
+    random_state: int
+    """Random number generator seed for random weight initialization."""
+
+    w_: np.ndarray
+    """Weights after fitting."""
+    b_: np.float_
+    """Bias unit after fitting."""
+    errors_: List[int]
+    """Number of misclassifications (updates) in each epoch."""
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1) -> None:
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        """Fit training data"""
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+        self.b_ = np.float_(0.0)
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_ += update * xi
+                self.b_ += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X: np.ndarray) -> np.ndarray:
+        """Calculate net input"""
+        return np.dot(X, self.w_) + self.b_
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
